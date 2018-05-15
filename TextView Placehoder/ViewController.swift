02a6f0c797ce92f730ce4d281003e20032bd0018
Karthick <karthick.c@technoduce.com>
2018-05-15 16:36:57 +0530

    TextView Placeholder
    TextView Placeholder like TextField

--- TextView Placehoder/ViewController.swift
+++ TextView Placehoder/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextViewDelegate {
+  @IBOutlet weak var textView: UITextView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    textView.placeholder = "Place holder"
+    textView.addDoneButtonOnKeyboard()
+    textView.layer.borderWidth = 1
+    textView.layer.borderColor = UIColor.black.cgColor
   }
 
   override func didReceiveMemoryWarning() {
